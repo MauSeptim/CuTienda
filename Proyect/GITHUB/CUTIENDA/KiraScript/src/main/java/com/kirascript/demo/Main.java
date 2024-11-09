373b3537a5b61c5071b92ff3d4631fa31e28aea9
Cristopher Celestino Martinez <cristopher.celestino25@gmail.com>
2024-11-09 01:58:18 -0600

    subir Version1

--- Proyect/GITHUB/CUTIENDA/KiraScript/src/main/java/com/kirascript/demo/Main.java
+++
@@ -1,9 +0,0 @@
-package com.kirascript.demo;
-
-import javafx.application.Application;
-
-public class Main {
-    public static void main(String[] args) {
-        Application.launch(TextEditor.class, args);
-    }
-}
--- Proyect/GITHUB/CUTIENDA/KiraScript/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.kirascript.demo {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.desktop;
-
-
-    opens com.kirascript.demo to javafx.fxml;
-    exports com.kirascript.demo;
-}
